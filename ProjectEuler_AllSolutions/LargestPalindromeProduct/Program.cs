3a2172658213f45cb737e8340a5263daec145460
dgormez <dgormez@gmail.com>
2017-03-10 15:56:31 +0100

    Problem 4 - Largest Palindrome from product of two digit numbers.

---
+++ ProjectEuler_AllSolutions/LargestPalindromeProduct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestPalindromeProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> allThreeDigitsProducts = new SortedSet<int>();
+
+            for (int i = 999; i >99; i--)
+            {
+                for (int j = 999; j > 99; j--)
+                {
+                    allThreeDigitsProducts.Add(i * j);
+                }
+            }
+
+            allThreeDigitsProducts.ToList().ToString();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
