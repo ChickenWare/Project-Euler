4cef7af52ffc8ddda7f15643d0038dda4bcde230
dgormez <dgormez@gmail.com>
2017-03-28 18:00:46 +0200

    Added solutions for problems  5 (Smallest multiple) and 6 (sum square difference)

---
+++ ProjectEuler_AllSolutions/SmallestMultiple/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxDivider = 20;
+            bool IsSmallestMultipleFound = false;
+            int answer = 0;
+
+            for (int i = maxDivider; !IsSmallestMultipleFound; i++)
+            {
+                for (int j = 2; j <= maxDivider; j++)
+                {
+                    if (!IsEvenlyDividable(i, j))
+                        break;
+
+                    if (j == maxDivider)
+                        IsSmallestMultipleFound = true;
+
+                }
+
+                if (IsSmallestMultipleFound)
+                    answer = i;
+            }
+
+            Console.WriteLine("SmallestMultiple = {0}", answer);
+            Console.ReadLine();
+        }
+
+        static bool IsEvenlyDividable(int number, int divider)
+        {
+            return number % divider == 0;
+        }
+    }
+}
---
+++ ProjectEuler_AllSolutions/SumSquareDifference/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumSquareDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            int answer = SquaresOfSumOfFirstXNaturalNumbers(number) - SumOfSquaresOfFirstXNaturalNumbers(number);
+
+            Console.WriteLine("Sum square difference = {0}", answer);
+            Console.ReadLine();
+        }
+
+        static int SumOfSquaresOfFirstXNaturalNumbers(int X)
+        {
+            int sumOfSquares = 0;
+            for (int i = 1; i <= X; i++)
+            {
+                sumOfSquares += i * i;
+            }
+            return sumOfSquares;
+        }
+
+        static int SquaresOfSumOfFirstXNaturalNumbers(int X)
+        {
+            int sum = 0;
+            for (int i = 1; i <= X; i++)
+            {
+                sum += i;
+            }
+            return sum * sum;
+        }
+    }
+}
