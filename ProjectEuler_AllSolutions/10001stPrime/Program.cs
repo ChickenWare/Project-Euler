c069282ec8dd96d19cb096319b92675df4f794b4
dgormez <dgormez@gmail.com>
2017-04-03 18:31:38 +0200

    7th problem done

--- ProjectEuler_AllSolutions/10001stPrime/Program.cs
+++ ProjectEuler_AllSolutions/10001stPrime/Program.cs
@@ -17,18 +17,66 @@ namespace _10001stPrime
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            int PrimeNumberPositionWeAreLookingFor = 10001;
+            List<int> allPrimeNumbers = FindPrimeNumbersBelow(105000);
+
+            Console.WriteLine("The 10001st prime number is: {0}", allPrimeNumbers[PrimeNumberPositionWeAreLookingFor-1]);
+            Console.ReadLine();
         }
 
-        static bool IsPrimeNumber(int number)
+        private static List<int> FindPrimeNumbersBelow(int number)
         {
-            double maxDivider = Math.Sqrt(number);
+            //double maxDivider = Math.Sqrt(number);
 
-            Dictionary<int,bool> listOfPotentialDivider = new Dictionary<int,bool>();
+            Dictionary<int, bool> AllNumbersToTest = ListAllNumbersBelow(number);
 
-            for (int i = 2; i <= maxDivider; i++)
+            foreach (var tmpNumberToTest in AllNumbersToTest.Keys.ToList())
+            {
+                
+                if (AllNumbersToTest[tmpNumberToTest] == true)
+                {
+                    //We are in the case it is still possible for the number to be prime
+                    if (!IsPrimeNumber(tmpNumberToTest))
+                    {
+                        //number is not prime
+                        AllNumbersToTest[tmpNumberToTest] = false; // It is not prime
+
+                        int tmpNumber = tmpNumberToTest;
+                        while (tmpNumber < number)
+                        {
+                            AllNumbersToTest[tmpNumber] = false;
+                            tmpNumber += tmpNumberToTest; // we will not check the multiples of a number which we know is not prime
+                        }
+
+                    }
+                }
+            }
+
+            List<int> allPrimeNumbers = AllNumbersToTest.Where(x => x.Value == true).Select(x => x.Key).ToList();
+            return allPrimeNumbers;
+        }
+
+        private static Dictionary<int, bool> ListAllNumbersBelow(int maxNumber)
+        {
+            Dictionary<int, bool> listOfPotentialDivider = new Dictionary<int, bool>();
+
+            for (int i = 2; i <= maxNumber; i++)
             {
                 listOfPotentialDivider.Add(i, true);
             }
+            return listOfPotentialDivider;
+        }
+
+        //This could be changed in a recursive using the algorithm above
+        private static bool IsPrimeNumber(int number)
+        {
+            double limit = Math.Sqrt(number);
+            for (int i = 2 ; i <= limit; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
