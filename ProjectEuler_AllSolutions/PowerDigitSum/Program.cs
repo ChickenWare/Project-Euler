08460e721e760f29ba763cc5889977be9b913f19
dgormez <dgormez@gmail.com>
2017-06-07 16:47:53 +0200

    Problem 16 OK

---
+++ ProjectEuler_AllSolutions/PowerDigitSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PowerDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger resultOfPower = 0;
+
+            resultOfPower = BigInteger.Pow(2, 1000);
+
+            string result = resultOfPower.ToString();
+
+            var resultAsNumberSequence = result.Select(c => c - '0').ToArray();
+
+            int sumOfPower = resultAsNumberSequence.Sum();
+        }
+    }
+}
