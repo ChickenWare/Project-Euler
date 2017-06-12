8c8685c2527e5ae79ec01984b8ba89a28ebad04e
dgormez <dgormez@gmail.com>
2017-06-12 15:28:21 +0200

    Problem 20 is OK

---
+++ ProjectEuler_AllSolutions/CountingSundays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSundays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = new DateTime(1901, 01, 1);
+            DateTime end = new DateTime(2000, 12, 31);
+
+            DateTime tmp = start;
+
+            int numberOfSundays = 0;
+
+            while (tmp != end)
+            {
+                if (tmp.DayOfWeek == DayOfWeek.Sunday && tmp.Day == 1)
+                    numberOfSundays++;
+
+                tmp = tmp.AddDays(1);
+            }
+
+            Console.WriteLine("Number of sundays = {0}",numberOfSundays);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectEuler_AllSolutions/FactorialDigitSum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Numerics;
+using Microsoft.SolverFoundation.Common;
+
+namespace FactorialDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger _result = 0;
+
+            _result = BigInteger.Factorial(100);
+
+            string result = _result.ToString();
+
+            var sumOfResultNumbers = result.Select(c => c - '0').ToArray();
+
+
+            Console.WriteLine("Sum of numbers in result: {0}",sumOfResultNumbers.Sum());
+            Console.ReadLine();
+        }
+    }
+}
